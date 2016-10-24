adbce4caac69246d56205c81473cf49c5e837ec9
marianoquevedo <marianorquevedo@gmail.com>
2016-10-23 21:26:07 -0300

    Deleted duplicate code in the validation schemas - typo

--- api/organizations/validation/updateOrganizationSchema.js
+++ api/organizations/validation/updateOrganizationSchema.js
@@ -1,20 +1,10 @@
 'use strict';
 
-const Joi = require('joi');
 const CreateOrganizationSchema = require('../validation/createOrganizationSchema');
 
 const internals = {};
 
-// internals.validTypes = ['employer', 'insurance', 'health system'];
-
-// internals.updateOrganizationSchema = Joi.object({
-//     name: Joi.string().min(2).max(120),
-//     description: Joi.string().min(2).max(400),
-//     url: Joi.string().uri(),
-//     code: Joi.string().alphanum(),
-//     type: Joi.string().valid(internals.validTypes).insensitive()
-// });
-
+// mark keys as optional to allow partial updates
 internals.updateOrganizationSchema = CreateOrganizationSchema.optionalKeys([
     'name', 'description', 'url', 'code', 'type']);
 
