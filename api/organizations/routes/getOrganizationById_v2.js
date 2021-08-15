07b7d5fb4ca9919b1590b231c8d641190eb8ea38
Mariano Quevedo <m.quevedo@klarna.com>
2021-08-15 15:44:03 +0200

    TEST-1 add code for PR

---
+++ api/organizations/routes/getOrganizationById_v2.js
@@ -0,0 +1,75 @@
+'use strict';
+
+const Joi = require('joi');
+const Boom = require('boom');
+const Util = require('util');
+const Organization = require('../model/Organization');
+
+const internals = {};
+
+internals.validateObjectId = require('../util/validateObjectId');
+
+internals.outputFields = require('../util/outputFields');
+
+internals.getOrganizationById = function (apiVersion, id) {
+
+    let outputFields = internals.outputFields;
+
+    // API Version 1 returns all the fields
+    if (apiVersion === 1) {
+        outputFields = Util._extend({
+            url: 1,
+            code: 1
+        }, internals.outputFields);
+    }
+
+    return Organization.findById(id, outputFields)
+      .then((org) => {
+
+          if (org === null) {
+              return Boom.notFound();
+          }
+          return org;
+      })
+      .catch((err) => {
+
+          return Boom.badImplementation(err);
+      });
+};
+
+internals.requestHandler = function (request, reply) {
+
+    const id = request.params.id;
+    const apiVersion = request.pre.apiVersion;
+
+    if (!internals.validateObjectId(id)){
+        return reply(Boom.badRequest('Must provide a valid organization id'));
+    }
+    return reply(internals.getOrganizationById(apiVersion, request.params.id));
+};
+
+module.exports = {
+    path: '/api/organizations/{id}',
+    method: 'GET',
+    handler: internals.requestHandler,
+    config: {
+        tags: ['api'],
+        description: 'Retrieve',
+        notes: 'Retrieves the details of an existing organization. You need to supply the unique organization identifier that was returned upon creation.',
+        validate: {
+            params: {
+                id: Joi.string().description('the organization id')
+            },
+            headers: Joi.object().keys({
+                Authorization: Joi.string()
+                                  .description('Authorization token')
+                                  .default('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwibmFtZSI6ImFkbWluIiwiaWF0IjoxNDc3NjI1MTU3fQ.ISQczg3OclurcE981oNuZVviVkhUZusAgO3uc3l0ExI'),
+
+                accept: Joi.string()
+                           .description('API version')
+                           .valid(['application/json', 'application/vnd.hapiservice.v1+json', 'application/vnd.hapiservice.v2+json'])
+                           .default('application/vnd.hapiservice.v2+json')
+            }).unknown()
+        }
+    }
+};
---
+++ api/organizations/routes/updateOrganizationById_2.js
@@ -0,0 +1,79 @@
+'use strict';
+
+const Joi = require('joi');
+const Boom = require('boom');
+const Organization = require('../model/Organization');
+const UpdateOrganizationSchema = require('../validation/updateOrganizationSchema');
+
+const internals = {};
+
+internals.validateObjectId = require('../util/validateObjectId');
+
+internals.setFieldsToUpdate = function (fieldName, payload, result) {
+
+    if (payload[fieldName]) {
+        result[fieldName] = payload[fieldName];
+    }
+};
+
+internals.updateOrganizationById = function (id, payload) {
+
+    const fieldsToUpdate = {};
+
+    internals.setFieldsToUpdate('name', payload, fieldsToUpdate);
+    internals.setFieldsToUpdate('description', payload, fieldsToUpdate);
+    internals.setFieldsToUpdate('code', payload, fieldsToUpdate);
+    internals.setFieldsToUpdate('url', payload, fieldsToUpdate);
+    internals.setFieldsToUpdate('type', payload, fieldsToUpdate);
+
+    if (fieldsToUpdate.type) {
+        fieldsToUpdate.type = fieldsToUpdate.type.toLowerCase();
+    }
+
+    return Organization.findByIdAndUpdate(id, {
+        $set: fieldsToUpdate
+    }, { new: true })
+      .then((org) => {
+
+          if (org === null) {
+              return Boom.notFound();
+          }
+          return org;
+      })
+      .catch((err) => {
+
+          return Boom.badImplementation(err);
+      });
+};
+
+internals.requestHandler = function (request, reply) {
+
+    const id = request.params.id;
+
+    if (!internals.validateObjectId(id)){
+        return reply(Boom.badRequest('Must provide a valid organization id'));
+    }
+    return reply(internals.updateOrganizationById(id, request.payload));
+};
+
+module.exports = {
+    path: '/api/organizations/{id}',
+    method: 'PUT',
+    handler: internals.requestHandler,
+    config: {
+        tags: ['api'],
+        description: 'Update',
+        notes: 'Updates the specified organization by setting the values of the parameters passed. Any parameters not provided will be left unchanged',
+        validate: {
+            payload: UpdateOrganizationSchema,
+            params: {
+                id: Joi.string().description('the organization id')
+            },
+            headers: Joi.object().keys({
+                Authorization: Joi.string()
+                                  .description('Authorization token')
+                                  .default('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwibmFtZSI6ImFkbWluIiwiaWF0IjoxNDc3NjI1MTU3fQ.ISQczg3OclurcE981oNuZVviVkhUZusAgO3uc3l0ExI')
+            }).unknown()
+        }
+    }
+};
